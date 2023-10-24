a927ead364ba2073cd8204b53012f97160802e42
daviinnchi <zaikoivan99@gmail.com>
2023-10-24 10:41:10 +0200

    Add and configure linter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (_, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+  // eslint-disable-next-line no-console
+  return console.log(`Express is listening at http://localhost:${port}`);
 });

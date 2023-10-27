efd4c1dbd4c144fc0fe615d5e3cba767d8a06c3f
davinnchii <zaikoivan99@gmail.com>
2023-10-27 11:08:36 +0200

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -6,6 +7,7 @@ const { productsRouter } = require('./routes/products.routes');
 
 const port = 3000;
 
+app.use(cors());
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });

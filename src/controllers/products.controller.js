0de050a2729f712daec7cc996e142b243d676799
davinnchii <zaikoivan99@gmail.com>
2023-10-27 22:35:40 +0200

    Add error handling

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,12 +1,27 @@
 const service = require('../services/products.service');
 
 const getAll = async(req, res) => {
+  const { offset, limit } = req.query;
+
+  if ((offset && Number.isNaN(+offset) || +offset < 0)
+    || (limit && Number.isNaN(+limit) || +limit < 0)) {
+    res.sendStatus(400);
+
+    return;
+  }
+
   const data = await service.getAll(req.query);
 
   res.send(data);
 };
 
 const getById = async(req, res) => {
+  if (Number.isNaN(req.params.id)) {
+    res.sendStatus(400);
+
+    return;
+  }
+
   const data = await service.get(+req.params.id);
 
   res.send(data);
